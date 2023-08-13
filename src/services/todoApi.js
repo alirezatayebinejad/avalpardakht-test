f13f96dbb82c512e5826e11f1445a6eba7d398a8
vibracode <codevibra@gmail.com>
2023-08-14 01:41:22 +0330

    api fix

--- src/services/todoApi.js
+++ src/services/todoApi.js
@@ -5,7 +5,7 @@ const BASE_URL = 'https://interview.aval.dev/api';
 async function addTodo(title, description, token) {
     try {
         const response = await axios.post(`${BASE_URL}/todo-list/add`, {
-            title,
+            todo: title,
             description,
         }, {
             headers: {
